c6b7d51bf5d71240fe058b606cc1aa9e20e028c3
System Administrator <root@benjamins-Mac.local>
2016-12-04 06:02:05 +0100

    04/12/2016

--- main.c
+++ main.c
@@ -1,98 +1,51 @@
 #include <stdio.h>
-#include <unistd.h>
-
-long int		ft_atoi(char *str)
-{
-	int		i;
-	int		sign;
-	int		result;
-
-	result = 0;
-	i = 0;
-	sign = 1;
-	while (str[i] >= 0 && str[i] <= 32)
-		i++;
-	if (str[i] == '-')
-		sign = -1;
-	if (str[i] == '+' || str[i] == '-')
-		i++;
-	while (str[i] && str[i] >= '0' && str[i] <= '9')
-	{
-		result = result * 10 + str[i] - '0';
-		i++;
-	}
-	return (sign * result);
-}
-
-int     main(int argc, char **argv)
+ 
+int     main(void)
 {
     int     tab[25];
-    int     etage;
+    int     etages;
     int     tmp;
     int     num;
-
+	int		min;
+ 
+	// initialisation du tableau
     tmp = 0;
-    num = ft_atoi(argv[1]);
+    num = 25;
+    while (tmp < 25)
+        tab[tmp++] = 0;
+
+	// compteur d'etages
+    tmp = 3;
+    etages = 1;
+    while (tmp < num)
+    {
+        tmp += (3 + etages);
+        etages++;
+    }
+
+	min = (num > 9) ? 3 : 2;
+
+	// initialisation du tableau
+	tmp = 0;
+	while (tmp < etages)
+		tab[tmp++] = min;
+
+	// recuperation du reste
 	int reste;
-	int i;
-//	write (1, "cpt\n", 4);
-		// compteur d'etage
-		tmp = 3;
-		etage = 1;
-		while (tmp < num)
-		{
-			tmp += (3 + etage);
-			etage++;
-		}
 
-		// initialisation du tableau
-//	write (1, "ini\n", 4);
-		tmp = 0;
-		while (tmp < 24)
-			tab[tmp++] = 0;
+	reste = num - (min * etages);
 
-		// initialisation du tableau
-//	write (1, "inj\n", 4);
-		tmp = 0;
-		while (tmp < etage)
-			if (num > 8)
-				tab[tmp++] = 3;
-			else
-				tab[tmp++] = 2;
-		tmp = 0;
-
-		// recuperation du reste
-//	write (1, "rst\n", 4);
-		if (num > 8)
-			reste = num - (3 * etage);
-		else
-			reste = num - (2 * etage);
-
-		// repartition du reste
-//	write (1, "rep\n", 4);
-		i = 0;
-		while (reste != 0)
-		{
-			i = etage - 1;
-			while ((tab[i - 1] != tab[i]) && tab[i])
-				i--;
-			if (tab[i] != 0)
-			{
-				tab[i] += 1;
-				reste--;
-			}
-		}
-
-		i = 0;
-	write (1, "rep\n", 4);
-		printf("%d ->", num);
-	write (1, "rep\n", 4);
-		while (tab[i] != 0)
-		{
-			printf(" %d", tab[i]);
-			i++;
-		}
+	// repartition du reste
+	int i = 1;
+	int val = min;
+	while (tab[i])
+	{
+		if (reste >= (etages - i) && ++val)
+			reste -= (etages - i);
+		tab[i++] = val; 
+	}
+	i = 0;
+	while (tab[i])
+		printf("%i\n", tab[i++]);
     return (0);
 }
-
-
