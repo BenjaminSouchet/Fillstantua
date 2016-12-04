4f676635f683b934b6fd9f8515136939f578d643
gmonein <mgaelbis@live.fr>
2016-12-04 04:59:53 +0100

    a voila

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int     main(void)
+long int		ft_atoi(char *str)
+{
+	int		i;
+	int		sign;
+	int		result;
+
+	result = 0;
+	i = 0;
+	sign = 1;
+	while (str[i] >= 0 && str[i] <= 32)
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return (sign * result);
+}
+
+int     main(int argc, char **argv)
 {
     int     tab[25];
     int     etage;
@@ -9,12 +32,10 @@ int     main(void)
     int     num;
 
     tmp = 0;
-    num = 5;
+    num = ft_atoi(argv[1]);
 	int reste;
 	int i;
-	while (num < 25)
-	{
-	write (1, "cpt\n", 4);
+//	write (1, "cpt\n", 4);
 		// compteur d'etage
 		tmp = 3;
 		etage = 1;
@@ -25,13 +46,13 @@ int     main(void)
 		}
 
 		// initialisation du tableau
-	write (1, "ini\n", 4);
+//	write (1, "ini\n", 4);
 		tmp = 0;
 		while (tmp < 24)
 			tab[tmp++] = 0;
 
 		// initialisation du tableau
-	write (1, "inj\n", 4);
+//	write (1, "inj\n", 4);
 		tmp = 0;
 		while (tmp < etage)
 			if (num > 8)
@@ -41,20 +62,20 @@ int     main(void)
 		tmp = 0;
 
 		// recuperation du reste
-	write (1, "rst\n", 4);
+//	write (1, "rst\n", 4);
 		if (num > 8)
 			reste = num - (3 * etage);
 		else
 			reste = num - (2 * etage);
 
 		// repartition du reste
-	write (1, "rep\n", 4);
+//	write (1, "rep\n", 4);
 		i = 0;
 		while (reste != 0)
 		{
 			i = etage - 1;
 			while ((tab[i - 1] != tab[i]) && tab[i])
-				i++;
+				i--;
 			if (tab[i] != 0)
 			{
 				tab[i] += 1;
@@ -71,8 +92,6 @@ int     main(void)
 			printf(" %d", tab[i]);
 			i++;
 		}
-		num++;
-	}
     return (0);
 }
 
