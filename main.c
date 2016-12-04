22a2b86a38951d8e851374aedfc80fd3378130a6
gmonein <mgaelbis@live.fr>
2016-12-04 04:09:20 +0100

    2016 04 12 04 10

--- main.c
+++ main.c
@@ -7,22 +7,49 @@ int     main(void)
     int     tmp;
     int     num;
  
+	// initialisation du tableau
     tmp = 0;
     num = 2;
     while (tmp < 25)
         tab[tmp++] = 0;
+
+	// compteur d'etage
     tmp = num;
-    while (num < 25)
+    tmp = 3;
+    etage = 1;
+    while (tmp < num)
     {
-        tmp = 3;
-        etage = 1;
-        while (tmp < num)
-        {
-            tmp += (3 + etage);
-            etage++;
-        }
-        printf("num = %i -- etage = %i\n\n", num ,etage);
-        num++;
+        tmp += (3 + etage);
+        etage++;
     }
+
+	// initialisation du tableau
+	tmp = 0;
+	while (tmp < etage)
+		if (num > 8)
+			tab[tmp++] = 3;
+		else
+			tab[tmp++] = 2;
+
+	// recuperation du reste
+	int reste;
+
+	tmp = 0;
+	if (num > 8)
+		reste = num - (3 * etage);
+	else
+		reste = num - (2 * etage);
+
+	// repartition du reste
+//	int i = (etage - 1);
+//
+//	while (reste != 0)
+//	{
+//		while (tab[i] + 1 != tab[i - 1])
+//			i++;
+//
+//	}
     return (0);
 }
+
+
