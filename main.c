50ce8f18121b88cd424d7a9b9c0a6096537c0dbe
System Administrator <root@benjamins-Mac.local>
2016-12-04 18:46:27 +0100

    04/12/2016

--- main.c
+++ main.c
@@ -1,51 +1,55 @@
 #include <stdio.h>
- 
-int     main(void)
+#include <stdlib.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int		*get_nb_floors(int num, int min, int rest)
 {
-    int     tab[25];
-    int     etages;
-    int     tmp;
-    int     num;
-	int		min;
- 
-	// initialisation du tableau
-    tmp = 0;
-    num = 25;
-    while (tmp < 25)
-        tab[tmp++] = 0;
+	int		i;
+	int		*tab;
+	int		floors;
 
-	// compteur d'etages
-    tmp = 3;
-    etages = 1;
-    while (tmp < num)
-    {
-        tmp += (3 + etages);
-        etages++;
-    }
-
-	min = (num > 9) ? 3 : 2;
-
-	// initialisation du tableau
-	tmp = 0;
-	while (tmp < etages)
-		tab[tmp++] = min;
-
-	// recuperation du reste
-	int reste;
-
-	reste = num - (min * etages);
-
-	// repartition du reste
-	int i = 1;
-	int val = min;
-	while (tab[i])
+	i = 3;
+	floors = 1;
+	if (!(tab = (int *)malloc(sizeof(int) * 25)))
+		return (NULL);
+	while (i < num)
 	{
-		if (reste >= (etages - i) && ++val)
-			reste -= (etages - i);
-		tab[i++] = val; 
+		i += (3 + floors);
+			floors++;
 	}
-	i = 0;
-	while (tab[i])
-		printf("%i\n", tab[i++]);
-    return (0);
+	min = (num > 5 && num != 8 && num != 9) ? 3 : 2;
+	rest = num - (min * floors);
+	i = 1;
+	tab[0] = min;
+	while (i < floors)
+	{
+		if (rest >= (floors - i) && ++min)
+			rest -= (floors - i);
+		tab[i++] = min;
+	}
+	tab[i] = 0;
+	return (tab);
+}
+
+int		main(void)
+{
+	int		i;
+	int		j;
+	int		*tab;
+
+	i = 3;
+	/*Affichage de verification (de 4 a 24)*/
+	while (++i <= 25)
+	{
+		j = 0;
+		printf("i : %i ---", i);
+		tab = get_nb_floors(i, 0, 0);
+		while (tab[j] != 0)
+			printf(" %i", tab[j++]);
+		printf("\n\n");
+		free(tab);
+	}
+	return (0);
 }
